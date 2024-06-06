46b67f44dfe085022e57cc02876d26d4c61d01b4
13virat <viratgupta1314@gmail.com>
2024-06-06 23:23:36 +0530

    add

--- djangoproject/djangoproject/urls.py
+++ djangoproject/djangoproject/urls.py
@@ -20,5 +20,11 @@ from stockmgmt import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path("list_items/", views.list_items, name='list_items'),
+    path("add_items/", views.add_items, name='add_items'),
+    # path("delete_items/", views.delete_items, name='delete_items'),
+    # path("update_items/", views.update_items, name='update_items'),
+    # path("search_items/", views.search_items, name='search_items'),
+    # path("stock_items/", views.stock_items, name='stock_items'),
     path("admin/", admin.site.urls),
 ]
--- djangoproject/stockmgmt/views.py
+++ djangoproject/stockmgmt/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
+from .models import *
+from .forms import StockCreateForm
 
 # Create your views here.
 def home(request):
-    title="Welcome to this Page"
+    title = "Welcome to this Page"
     context = {
         "title": title,
     }
-    return render(request, "home.html",context)
+    return render(request, "home.html", context)
 
 
+def list_items(request):
+    title = "List of Items"
+    queryset = Stock.objects.all()
+    context = {
+        "title": title,
+        "queryset": queryset,
+    }
+    return render(request, "list_items.html", context)
+
+
+def add_items(request):
+    form=StockCreateForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    content={
+        "form":form,
+        "title":"Add Items"
+    }   
+    return render(request, "add_items.html", content)
